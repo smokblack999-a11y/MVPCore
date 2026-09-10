9983b5404459a07d15d41bf5d14321426009892f
Samurai <smokblack999@gmail.com>
2026-09-11 03:42:53 +0500

    Centralize Media Hub integration constants

---
+++ sample/src/main/java/serg/chuprin/sample/media/MediaHubContract.java
@@ -0,0 +1,10 @@
+package serg.chuprin.sample.media;
+
+public final class MediaHubContract {
+    public static final String AUTHORITY_SUFFIX = ".fileprovider";
+    public static final String CACHE_DIR = "media";
+    public static final String IMAGE_TYPE = "image/*";
+    public static final String TELEGRAM_PACKAGE = "org.telegram.messenger";
+
+    private MediaHubContract() { }
+}
