1f0169a0f2867ad2b3ec0ba70f74aee53f0fcd5d
Samurai <smokblack999@gmail.com>
2026-09-19 01:49:16 +0500

    refactor: compose TDLib client without public transport leak

--- telegram-core/telegram-core-tdlib/src/main/java/telegram/core/tdlib/TdLibClientFactory.java
+++ telegram-core/telegram-core-tdlib/src/main/java/telegram/core/tdlib/TdLibClientFactory.java
@@ -1,13 +1,15 @@
 package telegram.core.tdlib;
 
 import telegram.core.api.TelegramClient;
-import telegram.core.api.TelegramFactory;
+import telegram.core.internal.DefaultTelegramClient;
+import telegram.core.internal.TelegramTransport;
 
 /** Composition root for the official TDLib JSON transport. */
 public final class TdLibClientFactory {
     private TdLibClientFactory() { }
 
     public static TelegramClient create(TdLibTransportConfig config) {
-        return TelegramFactory.create(new TdLibTransport(config));
+        TelegramTransport transport = new TdLibTransport(config);
+        return new DefaultTelegramClient(transport);
     }
 }
